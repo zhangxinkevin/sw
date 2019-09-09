7df4000a30cbfdffedd7c5ae3ecc9f8273b73351
52002015 <52002015@qq.com>
2019-09-09 15:34:11 +0800

    add PA cpu

---
+++ cpudata.go
@@ -0,0 +1,44 @@
+package sw
+
+import (
+	"log"
+	"time"
+
+	"github.com/gaochao1/gosnmp"
+)
+
+func CpuDataUtilization(ip, community string, timeout, retry int) (int, error) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println(ip+" Recovered in CPUtilization", r)
+		}
+	}()
+	vendor, err := SysVendor(ip, community, retry, timeout)
+	if err != nil {
+		return 0, err
+	}
+	method := "get"
+	var oid string
+
+	switch vendor {
+	case "PA_800", "PA":
+		oid = "1.3.6.1.2.1.25.3.3.1.2.2"
+	}
+
+	var snmpPDUs []gosnmp.SnmpPDU
+	for i := 0; i < retry; i++ {
+		snmpPDUs, err = RunSnmp(ip, community, oid, method, timeout)
+		if len(snmpPDUs) > 0 {
+			break
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	if err == nil {
+		for _, pdu := range snmpPDUs {
+			return pdu.Value.(int), err
+		}
+	}
+
+	return 0, err
+}
